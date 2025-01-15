194a8f095abd8e60442a3646461f47e005fde5eb
Leon (Jay) Kusznierz <contactunbee@gmail.com>
2025-01-15 14:48:46 +0100

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -18,7 +18,7 @@
       </div>
     </div>
     <div v-else class="now-playing" :class="getNowPlayingClass()">
-      <h1 class="now-playing__idle-heading">No music is playing 😔</h1>
+      <h1 class="now-playing__idle-heading">Put some Grooves on!</h1>
     </div>
   </div>
 </template>
